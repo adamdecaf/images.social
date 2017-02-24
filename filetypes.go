4954870b845160ae57c25594566462ba350a141f
Adam Shannon <adamkshannon@gmail.com>
2017-02-24 14:05:35 -0600

    properly decode images

--- filetypes.go
+++ filetypes.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 	"io"
 	"time"
 )
-
-// filetypes
-// - match on header info
-// - verify images are valid
-
-type ImageType int64
-func (i ImageType) Ext() string {
-	if i == JPEG { return ".jpeg" }
-	if i == PNG  { return ".png"  }
-	if i == GIF  { return ".gif"  }
-	return ""
-}
 
 const (
 	JPEG ImageType = iota
@@ -32,40 +20,26 @@ var (
 	ImageDetectionTimeout = 1000 * time.Millisecond
 )
 
+type ImageType int64
+func (i ImageType) Ext() string {
+	if i == JPEG { return ".jpeg" }
+	if i == PNG  { return ".png"  }
+	if i == GIF  { return ".gif"  }
+	return ""
+}
+
 func Detect(in io.Reader) ImageType {
-	out := make(chan ImageType)
-
-	// JPEG
-	go func() {
-		_, err := jpeg.Decode(in)
-		if err == nil {
-			out <- JPEG
+	_, format, err := image.Decode(in)
+	if err == nil {
+		if format == "jpeg" {
+			return JPEG
 		}
-		fmt.Println(err)
-	}()
-
-	// PNG
-	go func() {
-		_, err := png.Decode(in)
-		if err == nil {
-			out <- PNG
+		if format == "png"  {
+			return PNG
 		}
-		fmt.Println(err)
-	}()
-
-	// GIF
-	go func() {
-		_, err := gif.Decode(in)
-		if err == nil {
-			out <- GIF
+		if format == "gif"  {
+			return GIF
 		}
-		fmt.Println(err)
-	}()
-
-	select {
-	case tpe := <-out:
-		return tpe
-	case <-time.After(ImageDetectionTimeout):
-		return UNKNOWN
 	}
+	return UNKNOWN
 }
